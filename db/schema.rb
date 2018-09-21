25196814155cfcf6db5d62425a7aba9d788f7167
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2018-09-21 14:58:25 -0300

    Adicionando a quantidade de anúncios por categoria

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180919190222) do
+ActiveRecord::Schema.define(version: 20180921175129) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -50,8 +50,9 @@ ActiveRecord::Schema.define(version: 20180919190222) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "description", limit: 50
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
+    t.integer  "ads_count",              default: 0
   end
 
   create_table "members", force: :cascade do |t|
