50ca05a0a63904cb2a75e070c12ec3675a8d0b0b
Luiz <alexandre.lawb@gmail.com>
2018-10-04 18:58:48 +0000

    Ajustes no layout e outros pequenos ajustes

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -6,15 +6,20 @@
 /* Wiselinks Gem */
 $(function(){
   window.wiselinks = new Wiselinks($('#home-detail'));
+  
+  /* Retirar a propriedade de avaliação do ratyrate ao incluir a classe 'readonly-star' na div que engloba o ratyrate */
+  $(".readonly-star .star").each(function() {
+    $(this).raty('set', { readOnly: true });
+  });
 });
 
 /* Funções para resetar o carousel ao fazer o refresh da página */
 $('.carousel').carousel({
-    pause: true,
-    interval: false
+  pause: true,
+  interval: false
 }).carousel(0);
 
-$(window).on('load', function () {
-    var initial = $('ol.carousel-indicators li').attr('data-slide-to', 0);
-    initial.addClass('active');
+$(window).on('change', function () {
+  var initial = $('ol.carousel-indicators li').attr('data-slide-to', 0);
+  initial.addClass('active');
 });
